e5511ec4a0ac912595af9f76b674e4e2e8f0fce7
Anavailableuserna <158490512+Anavailableuserna@users.noreply.github.com>
2025-04-16 11:12:47 -0500

    deleted

--- flaskapp.py
+++ flaskapp.py
@@ -4,7 +4,6 @@ from flask import render_template
 from flask import Flask, render_template, request, redirect, url_for, flash
 from dbCode import *
 from UserInterface import *
-import mysql.connector
 
 app = Flask(__name__)
 app.secret_key = 'your_secret_key' 
