9b7a86c501c17d31ad3f77693993f56747a4d707
Anavailableuserna <158490512+Anavailableuserna@users.noreply.github.com>
2025-04-17 08:41:11 -0500

    genre JOIN

--- dbCode.py
+++ dbCode.py
@@ -21,7 +21,7 @@ def execute_query(query, args=()):
 
 def display_html(rows):
     html = ""
-    html += """<table><tr><th>movie_id</th><th>title</th><th>popularity</th></tr>"""
+    html += """<table><tr><th>movie_id</th><th>title</th><th>popularity</th><th>Genre</th></tr>"""
     for r in rows:
         html += "<tr><td>" + str(r[0]) + "</td><td>" + str(r[1]) + "</td><td>" + str(r[2]) + "</td><td>" + str(r[3]) + "</td></tr>"
     html += "</table></body>"
